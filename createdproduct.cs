a2cc95b39e866f4b2b30c7d3cf114998c29d33c4
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-11 11:56:10 +0400

    task1

---
+++ createdproduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            product phone = new product
+            {
+                Name="Iphone 13 pro max",
+                Brandname="Apple",
+                Price=1200,
+            };
+            Console.WriteLine("Name: "+phone.Name+"  Brandname: "+ phone.Brandname+"  Price: "+ phone.Price);
+        }
+    }
+
+    class product
+    {
+        public string Name;
+        public string Brandname;
+        public int Price;
+    }
+}
