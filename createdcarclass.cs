c62617b63e42f33005b8ba7e260cc9aed07f54ee
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-11 11:57:18 +0400

    task3

---
+++ createdcarclass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            car sale = new car
+            {
+                color="blue",
+                name="mercedes-benz",
+                brand="mercedes",
+                model="g63 amg",
+            };
+            Console.WriteLine("color :"+sale.color+"   "+"name:"+sale.name+"   "+"brand:"+ sale.brand + "   " +"model:"+ sale.model) ;
+
+        }
+    }
+
+    class vihicle
+    {
+        public string color;
+        public string name;
+    }
+
+    class car:vihicle
+    {
+        public string brand;
+        public string model;
+    }
+}
